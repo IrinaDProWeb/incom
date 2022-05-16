8e21a6d6886375e4314e1c72776e9a54fecd5ec6
IrinaDProWeb <98805290+IrinaDProWeb@users.noreply.github.com>
2022-05-16 20:50:27 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ $(function(){
     $('.tabs__block').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        prevArrow: "<img src='./images/prev-arrow.svg' class='prev' alt='1'>",
-        nextArrow: "<img src='./images/next-arrow.svg' class='next' alt='2'>",
+        prevArrow: "<img src='../images/prev-arrow.svg' class='prev' alt='1'>",
+        nextArrow: "<img src='../images/next-arrow.svg' class='next' alt='2'>",
 
         responsive: [
             {
